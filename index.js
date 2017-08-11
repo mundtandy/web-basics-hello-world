42326ded415a0e4c68fc80c0e68fd5b1dc318d24
mundtandy <andy.mundt@gmail.com>
2017-08-11 21:05:45 +1200

    updated

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ $(function() {
   // console.log will log a message or object to the browser developer console
   console.log("page loaded...");
 
-  $("selector-goes-here").click(/* function for when the button is clicked goes here */);
-  /*
-   * TODO: You will need to use a css selector to get jQuery to find the button element in the page
-   * Then you will need to make a new javascript function to do stuff for when the button
-   * is clicked and pass it into the click function above...
-   */
+  
+  $('#click').click(function(){
+	  alert(fetchRandomChuckNorrisJoke());
+  }
+  
+  );
+	
 });
+
 
 // Gets a random chuck norris joke from an API, then passes it to the callback function
 function fetchRandomChuckNorrisJoke(callback) {
